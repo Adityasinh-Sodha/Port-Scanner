8b5d3fb9cf09775a9e3ea3013bae634324c9d00e
Adityasinh <work.adityasodha@gmail.com>
2024-12-07 11:36:56 +0530

    Update PortScanner.py

--- PortScanner.py
+++ PortScanner.py
@@ -65,4 +65,4 @@ if __name__ == "__main__":
         file.write(f"Open Ports: {' '.join(map(str, open_ports))}\n")
 
     print(f"Open ports information written to '{OUTPUT_FILE}'")
-# created and edited by Aditya Sodha.
+# created and edited by Adityasinh.
